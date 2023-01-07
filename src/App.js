24317644cbe17d92931d864f340ccb9fef1a7a0b
= <=>
2023-01-07 16:18:40 +0700

    global filtering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css'
 import BasicTable from './components/BasicTable'
+import FilteringTable from './components/FilteringTable'
 import SortingTable from './components/SortingTable'
 
 function App() {
   console.log('huhuhuh')
   return (
     <div className="App">
-      <SortingTable />
+      <FilteringTable />
     </div>
   )
 }
---
+++ src/components/GlobalFilter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+//filter input component
+export const GlobalFilter = ({ filter, setFilter }) => {
+  return (
+    <span>
+      Search:{' '}
+      <input value={filter || ''} onChange={(e) => setFilter(e.target.value)} />
+    </span>
+  )
+}
