2812d6defcbd294a0d58409ed703fd2f07f9f396
= <=>
2023-01-08 02:51:37 +0700

    go to page

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -15,14 +15,17 @@ const PaginationTable = () => {
     nextPage,
     previousPage,
     canNextPage,
-    canPeviousPage,
+    canPreviousPage,
     pageOptions,
+    gotoPage,
+    pageCount,
     state,
     prepareRow,
   } = useTable(
     {
       columns,
       data,
+      initialState: { pageIndex: 2 }, //page ban dau khi chay app
     },
     usePagination,
   )
@@ -61,15 +64,34 @@ const PaginationTable = () => {
             {pageIndex + 1} of {pageOptions.length}
           </strong>
         </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(pageNumber)
+            }}
+            style={{ width: '50px' }}
+          />
+        </span>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>
+
         <button
           onClick={() => previousPage()}
-          disabled={!canPeviousPage}
+          disabled={!canPreviousPage}
           style={{ margin: '12px' }}
         >
           Previous
         </button>
         <button onClick={() => nextPage()} disabled={!canNextPage}>
           Next
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
         </button>
       </div>
     </div>
