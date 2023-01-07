71524d0f9a192cbc7e4f6aec729eec8d6ca2b6e5
= <=>
2023-01-08 03:30:55 +0700

    selecting rows

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css'
 import BasicTable from './components/BasicTable'
 import FilteringTable from './components/FilteringTable'
 import PaginationTable from './components/PaginationTable'
+import RowSelection from './components/RowSelection'
 import SortingTable from './components/SortingTable'
 
 function App() {
   console.log('huhuhuh')
   return (
-    <div className="App">
-      <PaginationTable />
+    <div>
+      <RowSelection />
     </div>
   )
 }
